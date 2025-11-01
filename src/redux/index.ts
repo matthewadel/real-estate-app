a4fac4e38ee51cceaf0286265773863562647ba9
matthew adel <matthewadel@matthews-MacBook-Pro.local>
2025-11-01 12:53:05 +0200

    first setup

---
+++ src/redux/index.ts
@@ -0,0 +1,12 @@
+import { createSlice } from "@reduxjs/toolkit";
+
+export const initialState = {};
+
+export const globalSlice = createSlice({
+  name: "global",
+  initialState,
+  reducers: {},
+});
+
+export default globalSlice.reducer;
+export const {} = globalSlice.actions;
